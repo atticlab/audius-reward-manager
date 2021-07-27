8022978e1e54a7a0ff4e95796a8112fff802a053
Vecheslav Druzhbin <vecheslavdruzhbin@gmail.com>
2021-07-27 14:12:35 +0300

    refactor: impl pack for states

--- program/src/state/reward_manager.rs
+++ program/src/state/reward_manager.rs
@@ -1,7 +1,12 @@
 use super::UNINITIALIZED_VERSION;
 use crate::PROGRAM_VERSION;
 use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{program_pack::IsInitialized, pubkey::Pubkey};
+use solana_program::{
+    msg,
+    program_error::ProgramError,
+    program_pack::{IsInitialized, Pack, Sealed},
+    pubkey::Pubkey,
+};
 
 /// Reward manager
 #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
@@ -17,9 +22,6 @@ pub struct RewardManager {
 }
 
 impl RewardManager {
-    /// The struct size on bytes
-    pub const LEN: usize = 66;
-
     /// Creates new `RewardManager`
     pub fn new(token_account: Pubkey, manager: Pubkey, min_votes: u8) -> Self {
         Self {
@@ -28,6 +30,25 @@ impl RewardManager {
             manager,
             min_votes,
         }
+    }
+}
+
+impl Sealed for RewardManager {}
+impl Pack for RewardManager {
+    // 1 + 32 + 32 + 1
+    const LEN: usize = 66;
+
+    fn pack_into_slice(&self, dst: &mut [u8]) {
+        let mut slice = dst;
+        self.serialize(&mut slice).unwrap()
+    }
+
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        Self::try_from_slice(src).map_err(|err| {
+            msg!("Failed to deserialize");
+            msg!(&err.to_string());
+            ProgramError::InvalidAccountData
+        })
     }
 }
 
